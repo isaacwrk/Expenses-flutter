9a34bcc8fb9b7e4a86f7133e8f01002f14a70502
Isaac Souza <isaacsouzawrk@gmail.com>
2021-06-22 12:14:47 -0300

    Inserindo pacote de converte de data

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 main()=>runApp(ExpensesApp());
 
@@ -86,7 +87,7 @@ class MyHomePage extends StatelessWidget {
                                 ),
                                 ),
                               Text(
-                                tr.date.toString(),
+                                DateFormat('d MMM y').format(tr.date),
                                 style:TextStyle(
                                   color:Colors.grey[600]
                                 )
