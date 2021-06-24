d69257011edb8f8533e13c1fbcb0fb0a816654e0
Isaac Souza <isaacsouzawrk@gmail.com>
2021-06-24 11:08:19 -0300

    Refatorando de Statless para Stateful

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 
-class TransactionForm extends StatelessWidget {
+class TransactionForm extends StatefulWidget {
   
-  final titleController = TextEditingController();
-  final valueController = TextEditingController();
-
   final void Function(String,double) onSubmit;
 
   TransactionForm(this.onSubmit);
+
+  @override
+  State<TransactionForm> createState() => _TransactionFormState();
+}
+
+class _TransactionFormState extends State<TransactionForm> {
+  final titleController = TextEditingController();
+
+  final valueController = TextEditingController();
+
     _submitForm(){
 
           final title =  titleController.text;
@@ -15,8 +22,9 @@ class TransactionForm extends StatelessWidget {
           if(title.isEmpty || value <= 0){
             return;
           }
-          onSubmit(title,value);
+          widget.onSubmit(title,value);
     }
+
   @override
   Widget build(BuildContext context) {
    return Card(
